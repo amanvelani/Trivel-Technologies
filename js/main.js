a200b625b8052aa2e409bc690b76561937843187
aman-3003 <59678544+aman-3003@users.noreply.github.com>
2021-05-17 11:00:21 +0530

    Added some animation to home page/ Contact page is working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.getElementById('button').addEventListener("click", function() {
 	document.querySelector('.bg-modal').style.display = "flex";
+	// document.querySelector('body').style.backgroundColor = "rgba(0.8, 0.8, 0.8, 0.8)"
 });
 
 document.querySelector('.close').addEventListener("click", function() {
